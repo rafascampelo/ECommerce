22295419b539bf065af4279a22df405034f492ea
rafaaacampelo <rafaccdfranca@gmail.com>
2025-06-24 17:39:10 -0300

    auth with clerk ready

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,14 +1,34 @@
 import Link from "next/link";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 function Navbar() {
   return (
-    <nav className="fixed top-0 w-full flex items-center py-2 px-8 justify-between z-50 bg-slate-800 text-gray-200 ">
+    <nav className="fixed top-0 w-full flex items-center py-2 px-8 justify-between z-50 bg-slate-800 text-gray-200">
       <Link
         href="/"
         className="uppercase font-bold text-md h-12 flex items-center"
       >
         Next Store
       </Link>
+
+      <div className="flex items-center gap-8">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <button className="border rounded-md border-gray-400 px-3 py-2">
+              Login
+            </button>
+          </SignInButton>
+        </SignedOut>
+      </div>
     </nav>
   );
 }
