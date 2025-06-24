b7dc19fea4f0ccf56386c3b852f91a976ba1d9d0
rafaaacampelo <rafaccdfranca@gmail.com>
2025-06-24 14:07:59 -0300

    page one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>next-ecommerce</h1>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-6"></div>
     </div>
   );
 }
